ae9ac53d1859862a8e2db22ec118ce484f5857c0
MMontemayor <mjmontemayor@hotmail.com>
2024-12-02 02:21:08 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+// Show game projects
+document.getElementById('gameToggle').addEventListener('click', function() {
+    document.getElementById('gameProjects').classList.remove('hidden');
+    document.getElementById('websiteProjects').classList.add('hidden');
+});
+
+// Show website projects
+document.getElementById('websiteToggle').addEventListener('click', function() {
+    document.getElementById('websiteProjects').classList.remove('hidden');
+    document.getElementById('gameProjects').classList.add('hidden');
+});
 
