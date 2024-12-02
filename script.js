cbfcd6300cb74fad40f12fb106939791b784865e
MMontemayor <mjmontemayor@hotmail.com>
2024-12-02 10:20:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
+// Show home page
+document.getElementById('homeToggle').addEventListener('click', function() {
+    document.getElementById('homePage').classList.remove('hidden');
+    document.getElementById('gameProjects').classList.add('hidden');
+    document.getElementById('websiteProjects').classList.add('hidden');
+});
+
 // Show game projects
 document.getElementById('gameToggle').addEventListener('click', function() {
     document.getElementById('gameProjects').classList.remove('hidden');
     document.getElementById('websiteProjects').classList.add('hidden');
+    document.getElementById('homePage').classList.add('hidden');
 });
 
 // Show website projects
 document.getElementById('websiteToggle').addEventListener('click', function() {
     document.getElementById('websiteProjects').classList.remove('hidden');
     document.getElementById('gameProjects').classList.add('hidden');
+    document.getElementById('homePage').classList.add('hidden');
 });
-
